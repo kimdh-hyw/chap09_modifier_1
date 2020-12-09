5add8fd99273d448c3f29938a197749c127c6ac0
jeffrey2465 <jeffrey2465@gmail.com>
2020-12-09 22:30:26 +0900

    Editing for publication

--- src/sec01_accessmodifier/EX02_AccessModifierOfClass_1/AccessModifierOfClass_1.java
+++ src/sec01_accessmodifier/EX02_AccessModifierOfClass_1/AccessModifierOfClass_1.java
@@ -5,8 +5,8 @@ package sec01_accessmodifier.EX02_AccessModifierOfClass_1;
 public class AccessModifierOfClass_1 {
 	public static void main(String[] args) {		
 		//#1. 객체 생성
-		A a = new A(); //객체생성가능(A a) + 생성자 호출 가능(new A())
-		B b = new B(); //객체생성가능(B b) + 생성자 호출 가능(new B())
-		C c = new C(); //객체생성가능(C c) + 생성자 호출 가능(new C())		
+		A a = new A(); //선언가능(A a) + 생성자 호출 가능(new A())
+		B b = new B(); //선언가능(B b) + 생성자 호출 가능(new B())
+		C c = new C(); //선언가능(C c) + 생성자 호출 가능(new C())		
 	}
 }
